6f2183e22a510a70865b6ce9a6699a2584ad121b
Hosseinzz95 <hosseinzarezadeh1995@gmail.com>
2025-12-02 23:20:49 +0330

    Set DEBUG=True for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-df52)r!p4$o&8re!w^^s=bsa48h3b88gyh%-uop6e)g9@a8lmn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['hosseinzarezadeh.onrender.com']
+DEBUG = True
+ALLOWED_HOSTS = ['hosseinzarezadeh.onrender.com', '*']
 
 
 
