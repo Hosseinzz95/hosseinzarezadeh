cb05a9c10265dec683cce7ddee3ef259d8f50f74
Hosseinzz95 <hosseinzarezadeh1995@gmail.com>
2025-12-02 23:45:28 +0330

    Fix static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,8 +126,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / 'core' / 'static' ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'   # for production collectstatic
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'core' / 'static',  # مسیر درست فایل‌های CSS/JS تو لوکال
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # مسیر جمع شدن همه static ها برای production
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
